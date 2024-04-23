71e19f986cca70dfa4b37cd0ce22d00e6a599dac
Raiyan109 <kraiyan109@gmail.com>
2024-04-23 18:13:38 +0600

    io

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('this is a realtime board app')
+})
 
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => {
-app.listen(process.env.PORT, (req, res) => {
+io.on('connection', (socket) => {
+    console.log('user connected');
+})
+
+server.listen(process.env.PORT, (req, res) => {
     console.log(`Server listening on ${process.env.PORT}`)
 })
-    // })
-    // .catch((error) => {
-    //     console.log(error)
-    // })
+
